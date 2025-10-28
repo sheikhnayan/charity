6acde64b0285bdd23a5c6e0e9a762bc23886eab7
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-10-28 06:25:38 +0600

    test

--- app/Models/UserSession.php
+++ app/Models/UserSession.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserSession extends Model
 {
-    //
+    protected $fillable = [
+        'session_id',
+        'user_id',
+        'ip_address',
+        'end_time',
+        'device_type',
+        'browser',
+        'ip_address',
+        'initial_referrer',
+        'landing_page',
+        'utm_parameters',
+    ];
+
+    /**
+     * Get the user that owns the session.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
