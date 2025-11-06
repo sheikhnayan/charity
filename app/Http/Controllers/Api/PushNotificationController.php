b0928fea5eaf07eafebe4455d03ad1696a1d4bb2
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-06 17:38:47 +0600

    test

--- app/Http/Controllers/Api/PushNotificationController.php
+++ app/Http/Controllers/Api/PushNotificationController.php
@@ -235,7 +235,7 @@ class PushNotificationController extends Controller
                 'general'
             );
 
-            dd($success);
+            // dd($success);
 
             return response()->json([
                 'success' => $success,
