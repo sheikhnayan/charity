28b4fa2b03f74365291232db307a1896beb2ea11
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-13 03:46:42 +0600

    test

--- app/Models/TicketSellDetail.php
+++ app/Models/TicketSellDetail.php
@@ -9,4 +9,8 @@ class TicketSellDetail extends Model
     public function ticket(){
         return $this->belongsTo(Ticket::class);
     }
+    
+    public function ticketSell(){
+        return $this->belongsTo(TicektSell::class, 'ticket_sell_id');
+    }
 }
