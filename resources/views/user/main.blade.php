f56e43d20d9df99b2ecd9865b1ce6b054a1ca9f7
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-20 12:46:46 +0600

    test

--- resources/views/user/main.blade.php
+++ resources/views/user/main.blade.php
@@ -63,7 +63,7 @@
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
+            <a href="/users/donation" class="app-brand-link">
               <span class="app-brand-logo demo">
                 <span class="text-primary">
                   <svg
