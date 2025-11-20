3dd1dc35fc10d5396fa91199ce150be8aca2a97b
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-20 13:08:46 +0600

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/heatmap/scroll',
             'api/heatmap/screenshot',
             'api/heatmap/screenshot/capture',
+            'users/investor-profile/save',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
