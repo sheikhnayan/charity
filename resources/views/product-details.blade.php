4bbc68bafe592476ba2de78f00f9cbcf98e6a6ad
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-22 01:10:01 +0600

    test

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -912,7 +912,6 @@
 
           <div class="section">
             <div class="seller-panel" style="margin-top:16px">
-              /* <h3 style="margin:0 0 8px">About this seller</h3> */
               <div class="seller-box">
                 <div class="avatar"> <img src="/uploads/{{$ticket->user->website->setting->logo}}" alt="{{$ticket->user->website->name}}'s avatar"> </div>
                 <div class="seller-meta">
