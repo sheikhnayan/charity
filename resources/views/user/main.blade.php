7dc350c0a6991d89d00caeb689484c695a777386
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-12-16 12:16:45 +0600

    test

--- resources/views/user/main.blade.php
+++ resources/views/user/main.blade.php
@@ -385,7 +385,7 @@
                   <div class="text-truncate" data-i18n="Email">Transactions</div>
                 </a>
               </li>
-              @if (Auth::user()->role == 'user' || Auth::user()->role == 'group_leader' || Auth::user()->role == 'parent' || Auth::user()->role == 'Parents')
+              @if (Auth::user()->role == 'user' || Auth::user()->role == 'group_leader' || Auth::user()->role == 'parent' || Auth::user()->role == 'Parents' || Auth::user()->role == 'parents')
                 <li class="menu-item {{ request()->is('users/student') ? 'active' : '' }}">
                     <a
                     href="/users/student"
