908e235fa50802e3ff4ee29e7777d82e3afd3aed
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-11 18:40:51 +0600

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -86,9 +86,9 @@
               <span class="app-brand-text demo menu-text fw-bold ms-2">Admin Panel</span>
             </a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            {{-- <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
+            </a> --}}
           </div>
 
           <div class="menu-inner-shadow"></div>
