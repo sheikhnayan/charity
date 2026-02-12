5864e46378f522880dc45800f314ae3eeeafdced
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-12 07:43:19 +0600

    test

--- resources/views/user/main.blade.php
+++ resources/views/user/main.blade.php
@@ -418,8 +418,8 @@
               <span class="app-brand-text demo menu-text fw-bold ms-2" style="font-size: 1rem;">Raise Builder</span>
             </a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
-              <i class="bx bx-chevron-left d-block d-xl-none align-middle"></i>
+            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+              <i class="bx bx-chevron-left align-middle"></i>
             </a>
           </div>
 
