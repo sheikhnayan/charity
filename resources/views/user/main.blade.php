a4eae1b00bde03c9b43313e608dd37849ad066a7
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-12 06:23:24 +0600

    test

--- resources/views/user/main.blade.php
+++ resources/views/user/main.blade.php
@@ -703,6 +703,7 @@
             <div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 d-xl-none">
               <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
                 <i class="icon-base bx bx-menu icon-md"></i>
+                Menu
               </a>
             </div>
 
