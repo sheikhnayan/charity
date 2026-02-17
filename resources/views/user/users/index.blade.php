27593e4b41b88696cba5f2f5cb8679494b3b60c1
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-18 01:02:22 +0600

    test

--- resources/views/user/users/index.blade.php
+++ resources/views/user/users/index.blade.php
@@ -228,6 +228,11 @@
                                     </tbody>
                                 </table>
                                 </div>
+                                
+                                <!-- Pagination Links -->
+                                <div class="d-flex justify-content-center mt-4">
+                                    {{ $users->links() }}
+                                </div>
                             </div>
                         </div>
                     </div>
