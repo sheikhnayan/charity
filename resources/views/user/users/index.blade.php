070aa17868fb071d8af9ed55c5aa1c7089dc01a6
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-26 21:11:34 +0600

    test

--- resources/views/user/users/index.blade.php
+++ resources/views/user/users/index.blade.php
@@ -335,7 +335,10 @@
 
         <!-- jQuery -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+        
+        <!-- DataTables JS (before CSS to ensure it loads) -->
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        
         <!-- DataTables CSS -->
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
@@ -346,19 +349,18 @@
             }
         </style>
 
-        <!-- DataTables and dependencies -->
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+        <!-- DataTables Buttons and dependencies -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 
         <script>
             $(document).ready(function() {
-                // Initialize DataTable with export/import buttons
-                var table = $('#usersTable').dataTable({
+                // Initialize DataTable
+                var table = $('#usersTable').DataTable({
                     dom: 'Bfrtip',
                     pageLength: 25,
                     scrollX: true,
@@ -368,7 +370,7 @@
                     buttons: [
                         'copy', 'csv', 'excel', 'pdf', 'print'
                     ]
-                }).api();
+                });
 
                 @if($isRoleUser)
                 // Custom search function for filtering
