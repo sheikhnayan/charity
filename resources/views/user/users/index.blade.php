2fc5caf6a51e46aa60699c3444f01b5ec60fad33
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-26 21:05:17 +0600

    test

--- resources/views/user/users/index.blade.php
+++ resources/views/user/users/index.blade.php
@@ -333,8 +333,7 @@
             @endif
         @endforeach
 
-        <!-- Include DataTables and jQuery CDN -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <!-- DataTables CSS -->
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
         <style>
@@ -343,18 +342,20 @@
                 color: #000 !important;
             }
         </style>
+
+        <!-- DataTables and dependencies -->
         <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 
         <script>
             $(document).ready(function() {
                 // Initialize DataTable with export/import buttons
-                let table = $('#usersTable').DataTable({
+                var table = $('#usersTable').DataTable({
                     dom: 'Bfrtip',
                     pageLength: 25,
                     scrollX: true,
