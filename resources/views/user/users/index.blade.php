22462a519fd4128b46b1e521539ab0ca5bec4b48
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-02-27 03:47:17 +0600

    test

--- resources/views/user/users/index.blade.php
+++ resources/views/user/users/index.blade.php
@@ -342,27 +342,17 @@
             }
         </style>
         
-        <!-- jQuery first -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        
-        <!-- DataTables Scripts -->
+        <!-- DataTables Scripts (jQuery already loaded in main layout) -->
         <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
         <script>
             $(document).ready(function() {
-                // Wait for DataTables to be fully loaded
-                if (typeof $.fn.DataTable === 'undefined') {
-                    console.error('DataTables not loaded yet, retrying...');
-                    setTimeout(arguments.callee, 100);
-                    return;
-                }
-                
                 const isRoleUser = {{ $isRoleUser ? 'true' : 'false' }};
                 
                 // Add custom search function before initializing DataTable
